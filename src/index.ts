db06d7f44c5e2ecab0b59a5c1a3497bee4c63140
keithchasen <keithchasen89@gmail.com>
2022-06-15 00:00:15 +0300

    middlewares added

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import { AppDataSource } from "./data-source";
 import express from 'express';
 import { CONST } from "./constants/strings";
+import cors from 'cors';
+import morgan from "morgan";
 
 AppDataSource.initialize().then(async () => {
     const app = express();
+
+    app.use(cors());
+    app.use(morgan('dev'));
 
     app.get('/', (req, res) => {
         res.send('hello')
