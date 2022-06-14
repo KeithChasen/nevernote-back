4fceb39a7c6846d9303d304da9b7fef16a1f24a0
keithchasen <keithchasen89@gmail.com>
2022-06-15 00:20:07 +0300

    typeorm User entity

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,18 +1,22 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
+import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from "typeorm"
+import { Field, ObjectType } from "type-graphql";
 
+@ObjectType()
 @Entity()
-export class User {
+export class User extends BaseEntity {
 
-    @PrimaryGeneratedColumn()
-    id: number
+    @Field(() => String)
+    @PrimaryGeneratedColumn('uuid')
+    id: string
+
+    @Field(() => String)
+    @Column()
+    email: string
+
+    @Field(() => String)
+    @Column()
+    username: string
 
     @Column()
-    firstName: string
-
-    @Column()
-    lastName: string
-
-    @Column()
-    age: number
-
+    password: string
 }
