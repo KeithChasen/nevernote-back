74861355934ce1cae278116c64db0bfef9cdf93e
keithchasen <keithchasen89@gmail.com>
2022-06-14 23:44:33 +0300

    basic express server

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,14 @@
 import { AppDataSource } from "./data-source";
+import express from 'express';
 
 AppDataSource.initialize().then(async () => {
-    console.log('App running');
+    const app = express();
+
+    app.get('/', (req, res) => {
+        res.send('hello')
+    })
+
+    app.listen(4000, () =>
+        console.log('server running http://localhost:4000')
+    )
 }).catch(error => console.log(error))
